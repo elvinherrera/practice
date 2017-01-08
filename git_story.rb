e6fa8029bf0f3d990cea6480f1b746f9ef56acbe
Elvin Herrera <nivle94@hotmail.com>
2017-01-08 15:17:38 -0500

    add new line and modified draft

--- git_story.rb
+++ git_story.rb
@@ -1,1 +1,3 @@
-once upon time i found andy in a starbucks and then showed him my elo and he cried (inside)
+once upon time i found andy in a mcdonalds and then showed him my elo and he cried (inside)
+
+and then he kept crying
